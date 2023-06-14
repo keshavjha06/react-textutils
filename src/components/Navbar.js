f98e5574badc8ed966dea01175c4657795064bdd
Keshav Jha <keshavjha0610@gmail.com>
2023-06-14 23:01:19 +0530

    modified Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,7 +23,7 @@ export default function Navbar(props) {
         <div className='collapse navbar-collapse' id='navbarSupportedContent'>
           <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
             <li className='nav-item'>
-              <Link className='nav-link active' aria-current='page' to='/home'>
+              <Link className='nav-link' aria-current='page' to='/home'>
                 Home
               </Link>
             </li>
@@ -33,20 +33,9 @@ export default function Navbar(props) {
               </Link>
             </li>
           </ul>
-          <form className='d-flex' role='search'>
-            <input
-              className='form-control me-2'
-              type='search'
-              placeholder='Search'
-              aria-label='Search'
-            />
-            <button className='btn btn-primary' type='submit'>
-              Search
-            </button>
-          </form>
-          <div className={`form-check form-switch text-${props.mode === 'light' ? `dark` : `light`}`}>
-            <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
+          <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+            <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault" />
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable DarkMode</label>
           </div>
         </div>
       </div>
